c7b1633e2838c23d3e1201067ea99c101d734799
Jiachi Liu <inbox@huozhi.im>
2022-03-08 22:05:14 +0100

    Fix custom _app

--- docs/pages/_app.mjs
+++ docs/pages/_app.mjs
@@ -1,11 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Head from 'next/head'
 
-export default function Document() {
+export default function App({ Component, pageProps }) {
   return (
-    <Html lang="en">
-      <Head />
-      {/* work around on head, title injection */}
-      <head>
+    <>
+      <Head>
         <title>sugar high</title>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -14,12 +12,8 @@ export default function Document() {
         <meta property="og:description" content="Super lightweight JSX syntax highlighter, around 1KB after minified and gzipped" />
         <meta name="twitter:image:src" content="https://repository-images.githubusercontent.com/453236442/9416c941-9c40-4dbe-a73e-0e0d21993802" />
         <meta name="twitter:card" content="summary_large_image" />
-      </head>
-      
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
