6de583d624a06de32826b1bc6f554a7e18c9342d
Jiachi Liu <inbox@huozhi.im>
2023-02-24 00:18:37 +0100

    Migrate to metadata API (#62)

--- docs/app/layout.jsx
+++ docs/app/layout.jsx
@@ -5,16 +5,7 @@ const imgUrl = 'https://repository-images.githubusercontent.com/453236442/1d63a6
 export default function Layout({ children }) {
   return (
     <html>
-      <head>
-        <title>sugar high</title>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="Sugar High" />
-        <meta property="og:description" content="Super lightweight JSX syntax highlighter, around 1KB after minified and gzipped" />
-        <meta property="og:image" content={imgUrl} />
-        <meta name="twitter:image" content={imgUrl} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </head>
+      <head></head>
       <body>
         <StyledJsxRegistry>
           {children}
@@ -22,4 +13,16 @@ export default function Layout({ children }) {
       </body>
     </html>
   )
+}
+
+export const metadata = {
+  title: 'Sugar High',
+  description: 'Super lightweight JSX syntax highlighter, around 1KB after minified and gzipped',
+  twitter: {
+    card: 'summary_large_image',
+    images: imgUrl,
+  },
+  openGraph: {
+    images: imgUrl,
+  },
 }
