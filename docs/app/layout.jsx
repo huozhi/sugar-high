35d724027a436ca0f86fb93605bd89eeabd4078a
Jiachi Liu <inbox@huozhi.im>
2023-02-24 00:49:58 +0100

    Add twitter and og info

--- docs/app/layout.jsx
+++ docs/app/layout.jsx
@@ -17,12 +17,17 @@ export default function Layout({ children }) {
 
 export const metadata = {
   title: 'Sugar High',
+  authors: [{ name: '@huozhi' }],
   description: 'Super lightweight JSX syntax highlighter, around 1KB after minified and gzipped',
   twitter: {
     card: 'summary_large_image',
     images: imgUrl,
+    title: 'Sugar High',
+    description: 'Super lightweight JSX syntax highlighter, around 1KB after minified and gzipped',
   },
   openGraph: {
     images: imgUrl,
+    title: 'Sugar High',
+    description: 'Super lightweight JSX syntax highlighter, around 1KB after minified and gzipped',
   },
 }
