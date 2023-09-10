d26f4e4c8c4d0ca3605aebb1a22e2804eb4a3fee
Jiachi Liu <inbox@huozhi.im>
2023-09-10 14:27:22 +0200

    add test

--- test/ast.test.js
+++ test/ast.test.js
@@ -195,6 +195,17 @@ describe('jsx', () => {
     expect(extractTokensTypes(tokens)).toEqual([
       'sign', 'identifier', 'identifier', 'sign', 'identifier', 'sign', 'string', 'string', 'string', 'sign',
     ])
+
+
+    const code2 = '<svg color="null" height="24"/>'
+    const tokens2 = tokenize(code2)
+    expect(extractTokenValues(tokens2)).toEqual([
+      '<', 'svg', 'color', '=', '"', 'null', '"', 'height', '=', '"', '24', '"', '/>',
+    ])
+    expect(extractTokensTypes(tokens2)).toEqual([
+      'sign', 'identifier', 'identifier', 'sign', 'string', 'string', 'string',
+      'identifier', 'sign', 'string', 'string', 'string', 'sign',
+    ])
   })
 })
 
