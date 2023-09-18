356c6077e26e85434d92f2df8493ff2e5611ae26
Jiachi Liu <inbox@huozhi.im>
2023-09-18 02:07:16 +0200

    docs: repo link

--- docs/app/components/install-banner.js
+++ docs/app/components/install-banner.js
@@ -3,7 +3,10 @@ import './install-banner.css'
 export default function InstallBanner() {
   return (
     <div className="install-banner">
-      <h1 className="install-banner__command">{`npm i sugar-high`}</h1>
+      <h1 className="install-banner__command">
+        {`npm i `}
+        <a href='https://github.com/huozhi/sugar-high' target='_blank' rel='noreferrer'>sugar-high</a>
+      </h1>
       <div className="max-width-container">
         <div className="install-banner__code">
           <code>
