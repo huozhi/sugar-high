4634c455f9a6a81dbf3d2eecdd20782a827b821b
Jiachi Liu <inbox@huozhi.im>
2024-10-25 20:45:26 +0200

    types: fix generate type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 export function highlight(code: string): string
 export function tokenize(code: string): Array<[number, string]>
-export function generate(tokens: string[]): Array<[number, string]>
+export function generate(tokens: Array<[number, string]>): Array<[number, string]>
