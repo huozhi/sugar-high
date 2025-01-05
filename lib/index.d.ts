1a3b18daf038c56bad496050199d57071138c2f9
Jiachi Liu <inbox@huozhi.im>
2025-01-05 19:47:36 +0100

    feat: accept keywords set (#110)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,7 @@
-export function highlight(code: string): string
+type HighlightOptions = {
+  keywords?: Set<string>
+}
+
+export function highlight(code: string, options?: HighlightOptions): string
 export function tokenize(code: string): Array<[number, string]>
 export function generate(tokens: Array<[number, string]>): Array<any>
--- test/ast.test.js
+++ test/ast.test.js
@@ -1,37 +1,11 @@
 import { describe, expect, it } from 'vitest'
-import { tokenize, SugarHigh } from '../lib'
-
-function getTypeName(token) {
-  return SugarHigh.TokenTypes[token[0]]
-}
-
-function getTokenValues(tokens) {
-  return tokens.map((tk) => tk[1])
-}
-
-function mergeSpaces(str) {
-  return str.trim().replace(/^[\s]{2,}$/g, ' ')
-}
-
-function filterSpaces(arr) {
-  return arr
-    .map(t => mergeSpaces(t))
-    .filter(Boolean)
-}
-
-function extractTokenValues(tokens) {
-  return filterSpaces(getTokenValues(tokens))
-}
-
-function getTokenArray(tokens) {
-  return tokens.map((tk) => [tk[1], getTypeName(tk)]);
-}
-
-function extractTokenArray(tokens) {
-  return tokens
-    .map((tk) => [mergeSpaces(tk[1]), getTypeName(tk)])
-    .filter(([_, type]) => type !== 'space' && type !== 'break')
-}
+import { tokenize } from '../lib'
+import { 
+  extractTokenArray, 
+  extractTokenValues,
+  getTokenValues,
+  getTokenArray,
+} from './testing-utils'
 
 describe('function calls', () => {
   it('dot catch should not be determined as keyword', () => {
