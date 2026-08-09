2f3b61219f5aefd51a2530f046a5344851a2a850
Jiachi Liu <inbox@huozhi.im>
2026-08-09 16:42:04 +0200

    Fix diff filename extension highlighting (#200)

--- packages/sugar-high/lib/presets/lang/diff.js
+++ packages/sugar-high/lib/presets/lang/diff.js
@@ -7,6 +7,11 @@ export const annotateLine = (line) => {
   if (line.value.startsWith('+') && !line.value.startsWith('+++')) annotation = 'diff-add'
   else if (line.value.startsWith('-') && !line.value.startsWith('---')) annotation = 'diff-remove'
   else if (line.value.startsWith('@@')) annotation = 'diff-hunk'
-  else if (/^(diff --git|index |--- |\+\+\+ )/.test(line.value)) annotation = 'diff-meta'
+  else if (/^(diff --git|index |--- |\+\+\+ )/.test(line.value)) {
+    annotation = 'diff-meta'
+    for (const token of line.tokens) {
+      if (token.type === 'property') token.type = 'identifier'
+    }
+  }
   if (annotation) line.annotations.push(annotation)
 }
